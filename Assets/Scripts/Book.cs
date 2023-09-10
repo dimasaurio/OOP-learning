d7f61d04b0840a8498243f7691e77c695d034c76
dimasaurio <dimasaurio1@hotmail.com>
2023-09-10 04:37:35 +0200

    Book Class
    Implemented book class with PrintedBook and EBook subclasses

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class Book : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private string title;
+    private string author;
+    private int year;
+    private string genre;
+
+    public string Title
     {
-        
+        get { return title; }
+        set { title = value; }
     }
 
-    // Update is called once per frame
-    void Update()
+    public string Author
     {
-        
+        get { return author; }
+        set { author = value; }
+    }
+
+    public int Year
+    {
+        get { return year; }
+        set { year = value; }
+    }
+
+    public string Genre
+    {
+        get { return genre; }
+        set { genre = value; }
+    }
+    public virtual void Read()
+    {
+        Debug.Log("Reading a book...");
+    }
+}
+
+public class PrintedBook : Book
+{
+    private int weight;
+
+    public int Weight
+    {
+        get { return weight; }
+        set { weight = value; }
+    }
+
+    private bool isDamaged;
+
+    public bool IsDamaged
+    {
+        get { return isDamaged; }
+        set { isDamaged = value; }
+    }
+    public override void Read()
+    {
+        Debug.Log("Flipping through the pages of a printed book...");
+    }
+
+}
+
+public class EBook : Book
+{
+    private string fileFormat;
+
+    public string FileFormat
+    {
+        get { return fileFormat; }
+        set { fileFormat = value; }
+    }
+
+    private bool isFree;
+
+    public bool IsFree
+    {
+        get { return isFree; }
+        set { isFree = value; }
+    }
+    public override void Read()
+    {
+        Debug.Log("Scrolling through the pages of an e-book...");
     }
 }
